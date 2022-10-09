ab87473b8bf2fdef7d93ecee14294e63ec2fff1b
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-10-09 13:02:13 +0900

    feat: set env

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,39 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const prevToken = localStorage.getItem("accessToken");
 	const clientId = process.env.REACT_APP_CLIENT_ID;
+	const apiKey = process.env.REACT_APP_API_KEY;
 	const url = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=https://localhost:3000/complete&scope=https://www.googleapis.com/auth/youtube.readonly&response_type=token`;
 
 	const onClickVideo = async () => {
-		const res = await axios.get(
-			"https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDqN5pmnzUDNhkfgDlF2m7iXNFkLWFwW-w"
-		);
+		const res = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${apiKey}`);
+		console.log(res.data);
+	};
 
-		console.log(res);
+	const onClickLogout = () => {
+		console.log("로그아웃");
 	};
 
 	useEffect(() => {
 		const verifyToken = () => {
 			axios
 				.get(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${prevToken}`)
-				.then((res) => console.log(res.data))
-				.catch((err) => alert("토큰만료"));
+				.then((res) => {
+					setIsLoggedIn(true);
+					console.log(res);
+				})
+				.catch((err) => {
+					localStorage.setItem("accessToken", "");
+					alert("토큰만료");
+					setIsLoggedIn(false);
+				});
 		};
+
 		if (prevToken) {
 			verifyToken();
 		}
 	}, [prevToken]);
 
 	return (
-		<div>
+		<>
 			<h1>youtube test</h1>
-			<button>
-				<a href={url}>유투브 계정인증</a>
-			</button>
-			<button onClick={onClickVideo}>비디오</button>
-		</div>
+			<div style={{ display: "flex", flexDirection: "column", width: "20%" }}>
+				{isLoggedIn ? (
+					<button onClick={onClickLogout}>로그아웃</button>
+				) : (
+					<button>
+						<a href={url}>유투브 계정인증</a>
+					</button>
+				)}
+				<button onClick={onClickVideo}>비디오</button>
+			</div>
+			{/* <iframe
+				width="560"
+				height="315"
+				src="https://www.youtube.com/embed/nMPCXuvL8EM"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowfullscreen
+			></iframe> */}
+		</>
 	);
 };
 
