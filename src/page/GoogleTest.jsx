e9894d95a4d26f817762582539051f955a7f34bb
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-11-14 13:11:40 +0900

    set env

--- src/page/GoogleTest.jsx
+++ src/page/GoogleTest.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const GoogleTest = () => {
-	const googleClientId = "628505447764-8ashfjb72t8tc9pq3ienf105viq9pdjs.apps.googleusercontent.com";
+	const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 	const googleRedirectUri = "http://localhost:3000/google";
 	const googlehref = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&include_granted_scopes=true&response_type=token&state=state_parameter_passthrough_value&redirect_uri=${googleRedirectUri}&client_id=${googleClientId}`;
 
